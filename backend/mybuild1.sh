3f99a3e3ca0b9a47d5be904e806dd668397ad066
kwonyul <kw990812@gmail.com>
2024-07-09 17:06:48 +0900

    커밋

--- backend/mybuild1.sh
+++ backend/mybuild1.sh
@@ -1,6 +1,6 @@
 # react project build
 cd ../frontend
-yarn build
+npm run build
 
 # index.html, main.js 복사(이동) : dist -> static
 cd ../backend
@@ -13,5 +13,15 @@ mv ../frontend/dist src/main/resources/static
 ./gradlew bootJar
 
 # build image
-docker build -t arbiterkdh1/saengjoncoding .
+docker build -t kwonyul99/prj3 .
+docker push kwonyul99/prj3
 
+
+# 컨테이너 멈추고
+ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.114.72 'docker stop saengjoncoding'
+# 컨테이너 삭제
+ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.114.72 'docker rm saengjoncoding'
+# pull image
+ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.114.72 'docker pull kwonyul99/prj3'
+# 컨테이너 실행
+ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.114.72 'docker run -d -p 8080:8080 --restart always --name saengjoncoding kwonyul99/prj3'
